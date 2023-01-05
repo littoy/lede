badc84c8db9f8ee4625182d7344f5acf3be177f5
fishg <1423545+fishg@users.noreply.github.com>
2023-01-05 11:49:17 +0800

    hv_utils: fix hv_utils autoload

--- package/kernel/linux/modules/netdevices.mk
+++ package/kernel/linux/modules/netdevices.mk
@@ -690,7 +690,7 @@ define KernelPackage/hv_utils
   DEPENDS:=@PCIE_SUPPORT +kmod-ptp
   KCONFIG:=CONFIG_HYPERV_UTILS
   FILES:=$(LINUX_DIR)/drivers/hv/hv_utils.ko
-  AUTOLOAD:=$(call AutoProbe,35,hv_utils,1)
+  AUTOLOAD:=$(call AutoLoad,35,hv_utils,1)
 endef
 
 define KernelPackage/hv_utils/description
