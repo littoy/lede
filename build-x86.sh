28811253e7b54746f4fc56ee75ddd37ff25247a0
littoy <103546957+littoy@users.noreply.github.com>
2023-03-13 11:08:42 +0800

    fix x86 sed replace

--- build-x86.sh
+++ build-x86.sh
@@ -48,7 +48,7 @@ fi
 #sed -i "s/CONFIG_DEFAULT_TARGET_OPTIMIZATION=\"-Os -pipe\"/CONFIG_DEFAULT_TARGET_OPTIMIZATION=\"-O2 -pipe -mtune=corei7\"/g" .config
 #sed -i "s/CONFIG_TARGET_OPTIMIZATION=\"-Os -pipe\"/CONFIG_TARGET_OPTIMIZATION=\"-O2 -pipe -mtune=corei7\"/g" .config
 #sed -i "s/CONFIG_CPU_TYPE=\" \"/CONFIG_CPU_TYPE=\"corei7\"/g" .config
-sed -i "s/CONFIG_TARGET_OPTIMIZATION=\"-Os -pipe\"/CONFIG_TARGET_OPTIMIZATION=\"-O2 -pipe\"/g" .config
+sed -i "s/-Os -pipe/-O2 -pipe/g" .config
 #rm -f /mnt/sdb1/x86lede/staging_dir/hostpkg/bin/ruby
 # sed -i 's/192.168.1./192.168.125./' .config
 # sed -i 's/192.168.125.1/192.168.125.2/' .config
